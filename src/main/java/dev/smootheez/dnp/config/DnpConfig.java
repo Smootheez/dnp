039fe9fbdfca73d5d97c2883928a745142cfd60d
Smootheez <raehanstars@gmail.com>
2025-07-30 08:58:25 +0700

    Removing default blacklist value from DnpConfig.

--- src/main/java/dev/smootheez/dnp/config/DnpConfig.java
+++ src/main/java/dev/smootheez/dnp/config/DnpConfig.java
@@ -13,5 +13,5 @@ public class DnpConfig {
     public static final ConfigOption<Boolean> HEALING = ConfigOption.create("healing", true);
     public static final ConfigOption<Double> DAMAGE_THRESHOLD = ConfigOption.create("damageThreshold", 10.0, 3.0, 50.0);
     public static final ConfigOption<Integer> PARTICLE_RADIUS = ConfigOption.create("particleRadius", 32, 16, 128).asSlider();
-    public static final ConfigOption<OptionList> BLACKLIST = ConfigOption.create("blacklist", "minecraft:allay");
+    public static final ConfigOption<OptionList> BLACKLIST = ConfigOption.create("blacklist");
 }
